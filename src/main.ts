6d87a40a70a6eb0b77ab6398f24d47dbee605888
hhvfg <hhvcg@hhvfg.local>
2020-09-02 07:28:17 +0800

    fix_jsonp

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
+import axios from 'axios'
+
+
 Vue.config.productionTip = false
 Vue.use(Antd);
+
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,41 @@
   <div class="home">
     <div><img alt="Vue logo" src="../assets/logo.jpg" width="300" height="200"></div>
     <div class="btn" @click="toNewoWorld"><a-button>wolcome</a-button></div>
+    <!-- <img src="http://127.0.0.1:5000/?callback=getdata"> -->
+
   </div>
 
 </template>
+
+
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Home extends Vue {
+  [x: string]: any;
+
+  private created(): void {
+    this.$axios({
+      method: "get",
+      // url: "https://api.coindesk.com/v1/bpi/currentprice.json"
+      url: "http://127.0.0.1:5000/test"
+    })
+      .then((res: any) => {
+        console.log('axios获取之数据：',res)
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+  }
+
   private toNewoWorld(): void  {
     this.$router.push({ path: '/One'})
+  }
+
+  private getdata(res: any): void {
+    console.log(res)
   }
 }
 </script>
