88bd2d6a9244f112e92e82cb89133b4afbcc4d9a
wangziqin <719713496@qq.com>
2020-09-27 12:31:54 +0800

    aft

--- src/components/Childtwo.vue
+++ src/components/Childtwo.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="hello">
-    <img src="https://f1.market.xiaomi.com/download/FoundApp/031dc74fcf1104c511e2c7788e11281d167a3399a/poster.png" class="poster" style="">
+    <img src="../assets/bg1.jpg" class="poster" style="">
     <button @click="handleVideo">click</button>
-    <video
-     poster="https://f1.market.xiaomi.com/download/FoundApp/031dc74fcf1104c511e2c7788e11281d167a3399a/poster.png" 
-     id="videoId7" 
-     webkit-playsinline="" 
-     playsinline="" 
-     preload="auto" 
-     x5-video-player-type="h5" 
-     x-webkit-airplay="true" 
-     class="video" 
-     src="https://f1.market.mi-img.com/download/FoundApp/0d5714d4de353ec14a86f04be204c52e07441a926/LLLLLL.mp4" 
-     style="width: 100vw; height: 56.272vw;"></video>
+    <video controls autoplay="true">
+       <source src="../assets/flowers.mp4" type="video/mp4">
+        <source src="../assets/flowers.webm" type="video/webm">
+     </video>
   </div>
 </template>
 
